085a423eabd025dd00503f25973f9651a5045c3c
Dino Tavares <104394676+din00tavares-facily@users.noreply.github.com>
2023-08-02 09:26:11 -0300

    Update script.js
    Comentário sobre resultado

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function askForVm(){
 
         rl.question('Digite o tempo de viagem: ', (temp) => {
             const tempoDeViagem = temp;
-    
+            // Do jeito que tá funciona, mas eu colocaria o resultado numa variável e faria a formatação e arredondamento do número na hora de fazer o console.log
             console.log(`A velocidade média da viagem é ${espacoPercorrido/tempoDeViagem} km/h`);
     
             rl.close();
