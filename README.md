efc43a61a669e7d81311ee97aeae62c0470dc84d
André Gabriel Odon de Freitas <andre.odon@fcptn.local>
2023-08-02 15:31:49 -0300

    melhorias na formação dos dados impressos na tela

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 Este projeto visa a minha prática em Node.JS, onde inserindo a distância e o tempo necessários, o cálculo é feito e mostra a velocidade média necessária para a viagem.
 
+Fiz melhorias no projeto. Foi uma sugestão do meu atual mentor [Dino Tavares](https://github.com/din00tavares-facily).
+
 Este projeto ainda está em desenvolvimento e meu objetivo e implementar interface gráfica para desktop e torná-lo arquivo executável para que qualquer pessoa possa instalar em seu computador.
--- script.js
+++ script.js
@@ -5,16 +5,22 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+console.log('=========================================================================================================')
+console.log('===================================CALCULADORA DE VELOCIDADE MÉDIA=======================================')
+console.log('=========================================================================================================')
+
 function askForVm(){
-    rl.question('Digite a distância a ser percorrida: ', (esp) => {
+    rl.question('Digite a distância a ser percorrida em quilômetros: ', (esp) => {
         const espacoPercorrido = esp;
 
         // console.log(`Seu nome é ${nomeArmazenado}`);
 
-        rl.question('Digite o tempo de viagem: ', (temp) => {
+        rl.question('Digite o tempo de viagem em horas: ', (temp) => {
             const tempoDeViagem = temp;
+
+            vm = espacoPercorrido / tempoDeViagem;
     
-            console.log(`A velocidade média da viagem é ${espacoPercorrido/tempoDeViagem} km/h`);
+            console.log(`A velocidade média da viagem é ${vm.toFixed(2)} km/h`);
     
             rl.close();
         });
